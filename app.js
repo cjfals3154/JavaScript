20af239d4dd7c996dc46351a4691acb8ff38be56
Shincheolmin <90400739+cjfals3154@users.noreply.github.com>
2022-01-17 22:41:24 +0900

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+// function diveide(a, b){
+//     console.log(a / b);
+// }
+
+// diveide(60, 8);
+
+const player = {
+    name: "nico",
+    sayHello: function( a ){
+        console.log("hello" + a);
+    },
+};
+
+player.sayHello("shin")
